69f4714468dbc7e89a237e29359cd9709e72eba4
Nicolas S <usuario1@VMPruebas>
2025-10-22 06:45:03 +0000

    Resolucion Punto F

---
+++ Punto_F.sh
@@ -0,0 +1,4 @@
+# Para solicitar la ip publica: curl -s ifconfig.me
+# mkdir -p Punto_F
+# creacion de archivo .txt: cat << FIN > Filtro_Avanzado.txt
+# Informacion sobre cpu, misma ruta del punto anterior E).
