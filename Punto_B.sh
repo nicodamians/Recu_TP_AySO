6681f59eb19795e57bcf460fa028aaea105641a4
Nicolas S <usuario1@VMPruebas>
2025-10-22 04:56:04 +0000

    Resolucion Punto B

---
+++ Punto_B.sh
@@ -0,0 +1,6 @@
+# sudo useradd -m -s /bin/bash p1c1_2024_u1
+# sudo passwd p1c1_2024_u1
+# sudo groupadd p1c1_2024_g1
+# sudo usermod -G p1c1_2024_g1 p1c1_2024_u1 (grupo secundario)
+# sudo chown p1c1_2024_u1 datos
+# sudo chmod 750 datos/
