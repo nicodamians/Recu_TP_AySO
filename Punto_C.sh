2f03c3c3bf471c8b31b7324d360b9fffcfef24eb
Nicolas S <usuario1@VMPruebas>
2025-10-22 06:04:22 +0000

    Resolucion Punto C

---
+++ Punto_C.sh
@@ -0,0 +1,8 @@
+# creacion de usuario copiando contraseña del anterior: sudo useradd -m -s /bin/bash -p ( utilizando filtro de shadow, grep. awk -F y | para copiar y pegar.
+# mismo comandos para creacion de grupo.
+# sudo usermod -g p1c1_2024_Todos p1c1_2024_u2
+# sudo usermod -aG p1c1_2024_Todos p1c1_2024_u1
+
+# Cambiar autoria de directorio: sudo chown :p1c1_2024_Todos datos/
+# Cambiar permisos: chmod 724 datos/
+
